372195d4b6a40d0b0cbca8980ab6fa7ee926f35d
Christopher Palmer <chris.palmer@magnolia-river.com>
2020-07-27 20:28:26 -0500

    Added a branch to try out tableviews

--- CollectionViewSample/CollectionViewSample/MainPage.xaml.cs
+++ CollectionViewSample/CollectionViewSample/MainPage.xaml.cs
@@ -19,18 +19,28 @@ namespace CollectionViewSample
             InitializeComponent();
             MyCollectionView.BindingContext = this;
             MyCollectionView.SetBinding(ItemsView.ItemsSourceProperty, nameof(ControlList));
+
+            for (var x = 0; x < 20; x++)
+            {
+                // ControlList.Add(new Label {Text = $"Added at {ControlList.Count}"});
+                MainSection.Add(new ViewCell {View = new Label {Text = $"Added at {MainSection.Count}"}});
+            }
         }
 
         public ObservableCollection<View> ControlList { get; set; } = new ObservableCollection<View>();
 
         private void Button1_OnClicked(object sender, EventArgs e)
         {
-            ControlList.Add(new Label {Text = $"Added at {ControlList.Count}"});
+            // ControlList.Add(new Label {Text = $"Added at {ControlList.Count}"});
+
+            MainSection.Add(new ViewCell {View = new Label {Text = $"Added at {MainSection.Count}"}});
         }
 
         private void Button2_OnClicked(object sender, EventArgs e)
         {
-            ControlList.Add(new Entry {Text = $"Added at {ControlList.Count}"});
+            // ControlList.Add(new Entry {Text = $"Added at {ControlList.Count}"});
+
+            MainSection.Add(new ViewCell {View = new Entry {Text = $"Added at {MainSection.Count}"}});
         }
     }
 }
